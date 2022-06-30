cf46b69d537cbf69814b68d9f32225ae606938ee
gianlucapili93 <gianlucapili93@gmail.com>
2022-06-30 12:47:49 +0200

    Add project files.

---
+++ TryEvents/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static System.Console;
+
+namespace TryEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitcoin = new Bitcoin(1000);
+            var user1 = new User();
+            user1.Buy(bitcoin); //event subscription
+
+            WriteLine("Change price: ");
+            string newprice = ReadLine();
+            bitcoin.Price = int.Parse(newprice);
+
+            user1.Sell(bitcoin);
+            WriteLine("Change price: ");
+            newprice = ReadLine();
+            bitcoin.Price = int.Parse(newprice);
+        }
+
+        //Event Handler 
+        public static void PriceChange(object sender, PriceEventArgs e)
+        {
+            WriteLine($"Price change: {e.NewPrice}");
+        }
+
+        public class PriceEventArgs : EventArgs
+        {
+            public int NewPrice { get; set; }
+            public PriceEventArgs(int price)
+            {
+                NewPrice = price;
+            }
+        }
+
+        public interface Crypto
+        {
+            public event EventHandler<PriceEventArgs> PriceChanged;
+        }
+
+        public class Bitcoin : Crypto //Publisher
+        {
+            public event EventHandler<PriceEventArgs> PriceChanged;
+            private static int price;
+            public int Price
+            {
+                get { return price; }
+                set
+                {
+                    price = value;
+                    OnPriceChange(new PriceEventArgs(value));
+                }
+            }
+            public Bitcoin(int price)
+            {
+                Price = price;
+            }
+            protected void OnPriceChange(PriceEventArgs e)
+            {
+                PriceChanged?.Invoke(this, e); //invoke event
+            }
+        }
+        public class User //Subscriber
+        {
+            public void Buy(Crypto crypto)
+            {
+                crypto.PriceChanged += PriceChange; //subscription
+            }
+            public void Sell(Crypto crypto)
+            {
+                crypto.PriceChanged -= PriceChange; //desubscription
+            }
+        }
+    }
+}
